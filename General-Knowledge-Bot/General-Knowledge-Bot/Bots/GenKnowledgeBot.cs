5cb4defbc8aaef2dea5428e6be17e157b4fe73f5
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-06-06 11:38:21 -0700

    Having the necessary changes made such that the welcome adaptive card can be sent out

--- General-Knowledge-Bot/General-Knowledge-Bot/Bots/GenKnowledgeBot.cs
+++ General-Knowledge-Bot/General-Knowledge-Bot/Bots/GenKnowledgeBot.cs
@@ -6,8 +6,10 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using General_Knowledge_Bot.Helpers.AdaptiveCards;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
 
 namespace General_Knowledge_Bot.Bots
 {
@@ -24,9 +26,21 @@ namespace General_Knowledge_Bot.Bots
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and Welcome!"), cancellationToken);
+                    var welcomeCardAttachment = CreateWelcomeCardAttachment();
+                    await turnContext.SendActivityAsync(MessageFactory.Attachment(welcomeCardAttachment), cancellationToken);
                 }
             }
+        }
+
+        private static Attachment CreateWelcomeCardAttachment()
+        {
+            var cardString = WelcomeMessageAdaptiveCard.GetCard();
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(cardString)
+            };
+            return adaptiveCardAttachment;
         }
     }
 }
--- General-Knowledge-Bot/General-Knowledge-Bot/Helpers/AdaptiveCards/WelcomeMessageAdaptiveCard.cs
+++ General-Knowledge-Bot/General-Knowledge-Bot/Helpers/AdaptiveCards/WelcomeMessageAdaptiveCard.cs
@@ -1,11 +1,39 @@
 ﻿namespace General_Knowledge_Bot.Helpers.AdaptiveCards
 {
-    using System;
+    using General_Knowledge_Bot.Properties;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
+    using System.IO;
 
     public class WelcomeMessageAdaptiveCard
     {
+        public static string CardTemplate;
+
+        static WelcomeMessageAdaptiveCard()
+        {
+            var cardJsonFilePath = Path.Combine(".", "Helpers", "AdaptiveCards", "WelcomeMessageAdaptiveCard.json");
+            CardTemplate = File.ReadAllText(cardJsonFilePath);
+        }
+
+        public static string GetCard()
+        {
+            var welcomeCardTitleText = Resource.WelcomeCardTitleText;
+            var welcomeCardContentPart1 = Resource.WelcomeCardContentPart1;
+            var welcomeCardContentPart2 = Resource.WelcomeCardContentPart2;
+
+            var variablesToValues = new Dictionary<string, string>()
+            {
+                { "welcomeCardTitleText", welcomeCardTitleText },
+                { "welcomeCardContentPart1", welcomeCardContentPart1 },
+                { "welcomeCardContentPart2", welcomeCardContentPart2 }
+            };
+
+            var cardBody = CardTemplate;
+            foreach (var kvp in variablesToValues)
+            {
+                cardBody = cardBody.Replace($"%{kvp.Key}%", kvp.Value);
+            }
+
+            return cardBody;
+        }
     }
 }
