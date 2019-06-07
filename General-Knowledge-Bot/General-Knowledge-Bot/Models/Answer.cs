fa32a8b5e1e6f4a21bfdae34a217c8f891d63c93
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-06-06 18:58:31 -0700

    Making sure that all of the changes are coming through for the final stage - answer adaptive cards

---
+++ General-Knowledge-Bot/General-Knowledge-Bot/Models/Answer.cs
@@ -0,0 +1,11 @@
+﻿namespace GeneralKnowledgeBot.Models
+{
+    public class Answer
+    {
+        public string[] questions { get; set; }
+        public string answer { get; set; }
+        public double score { get; set; }
+        public string source { get; set; }
+        public Context context { get; set; }
+    }
+}
