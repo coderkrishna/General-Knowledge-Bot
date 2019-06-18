4c2a624cee5f9cd1d6cc426e88325fdfadb6fc4f
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-06-18 11:23:52 -0700

    Having the share app feedback adaptive card done

---
+++ General-Knowledge-Bot/General-Knowledge-Bot/Helpers/AdaptiveCards/ShareAppFeedbackAdaptiveCard.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="ShareAppFeedbackAdaptiveCard.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace GeneralKnowledgeBot.Helpers.AdaptiveCards
+{
+    /// <summary>
+    /// The class for generating the share app feedback adaptive card.
+    /// </summary>
+    public class ShareAppFeedbackAdaptiveCard
+    {
+    }
+}
